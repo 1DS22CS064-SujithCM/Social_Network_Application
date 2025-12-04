f2d7895562faa4c741f4b3772e3814816a21d9c1
1DS22CS064-SujithCM <136790929+1DS22CS064-SujithCM@users.noreply.github.com>
2025-12-04 21:10:56 +0530

    Update SocketContext.jsx

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -3,7 +3,7 @@ import io from "socket.io-client";
 import { UserData } from "./UserContext";
 
 // const EndPoint = "https://mern-social-3e3m.onrender.com";
-const EndPoint = "https://social-network-application-wqi9.onrender.com"; // or 8000 or the port your backend uses
+const EndPoint = "https://social-network-application-1.onrender.com"; // or 8000 or the port your backend uses
 
 
 const SocketContext = createContext();
